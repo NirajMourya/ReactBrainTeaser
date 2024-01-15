4b846a78259d7677e49a22eb8185ca7fdcb32fbe
NirajMourya <nirajmourya786@gmail.com>
2024-01-16 01:40:17 +0530

    Added answer shuffle and quiz completed logic

--- reactbrainteaser/src/components/Quiz.jsx
+++ reactbrainteaser/src/components/Quiz.jsx
@@ -1,24 +1,40 @@
 import { useState } from "react";
 import QUESTIONS from "../questions"
+import quizCompleteImg from '../assets/quiz-complete.png';
 
 export default function Quiz()
 {
     const [userAnswers,setUserAnswers] = useState([]);
     const activeQuestionIndex = userAnswers.length;
-
+    const quizIsCompleted = activeQuestionIndex === QUESTIONS.length;
+    
     function handleSelectAnswer(selectedAnswer)
     {
         setUserAnswers((prevAnswers) => {
             return [...prevAnswers, selectedAnswer]
         })
     }
+    
+    if( quizIsCompleted )
+    {
+        return (
+            <div id="summary">
+                 <img src={quizCompleteImg} alt="Trophy Icon"/>
+                 <h2>Quiz Completed!</h2>
+            </div>
+        )
+    }
+
+    const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers]
+    shuffledAnswers.sort((a,b) =>  Math.random() - 0.5);
 
     return (
+        <div id="quiz">
         <div id="question">
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
             {
-                QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+                shuffledAnswers.map((answer) => (
                    <li key={answer} className="answer">
                       <button onClick={() => handleSelectAnswer(activeQuestionIndex)}>
                          {answer}
@@ -27,6 +43,7 @@ export default function Quiz()
                 ))
             }
         </ul>
+        </div>
         </div>
     );
 }
